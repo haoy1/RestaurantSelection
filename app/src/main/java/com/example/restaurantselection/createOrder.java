b5666640d99c981b0fcacd2c8bb16b118560c5c2
chenxiany11 <chenx11@rose-human.edu>
2020-05-08 01:39:45 -0400

    make create order button work

---
+++ app/src/main/java/com/example/restaurantselection/createOrder.java
@@ -0,0 +1,13 @@
+package com.example.restaurantselection;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class createOrder extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_order_layout);
+    }
+}
